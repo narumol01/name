d7529d2e66bad12154d57d0dd65de1f68ddcf4ba
narumol01 <73689289+narumol01@users.noreply.github.com>
2020-11-20 19:11:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -30,6 +30,15 @@ while($Result = mysqli_fetch_array($res))
 ?>
 </table>
 <?php
+<form action = "insert.php" method = "post" id="CommentForm" >
+    Name:<br>
+    <input type="text" name = "name" id="idName" placeholder="Enter Name"> <br>
+    Comment:<br>
+    <textarea rows="10" cols="20" name = "comment" id="idComment" placeholder="Enter Comment"></textarea><br>  
+    Link:<br>
+    <input type="text" name = "link" id="idLink" placeholder="Enter Link"> <br><br>
+    <input type="submit" id="commentBtn">
+</form> 
 $name = $_POST['name'];
 $comment = $_POST['comment'];
 $link = $_POST['link'];
